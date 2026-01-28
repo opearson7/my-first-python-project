b35f6dfa4cce9f1e37a28677af8ef9bca7f3067b
opearson7 <oliviaisabella18@gmail.com>
2026-01-27 19:34:55 -0500

    Update My first Python program - Liv Pearson .py

--- My first Python program - Liv Pearson .py
+++ My first Python program - Liv Pearson .py
@@ -1,5 +1,5 @@
 print("Welcome to SDEV 120 class!")
 name = input("Enter your name: ")
 print("Hello, " + name) 
-git remote add origin [https://github.com/opearson7/my-first-python-project.git]
+
 
