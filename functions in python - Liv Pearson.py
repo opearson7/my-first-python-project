a796f3e281d2374f47380bf2a3839e8d42b7696e
opearson7 <oliviaisabella18@gmail.com>
2026-02-27 22:41:51 -0500

    Add files via upload

---
+++ functions in python - Liv Pearson.py
@@ -0,0 +1,11 @@
+def greater_than(x,y):
+    if x > y:
+        print("True")
+    else:
+        print("False")
+
+x = 10
+y = 6
+
+greater_than(10,6)
+print("The statement '" + str(x) + "' is more significant than '" + str(y) + "' is " + str(x > y))
